4b5d714a39ea979716ffeec533128851655f9a18
paulords95 <paulords95@gmail.com>
2020-07-12 14:53:49 -0300

    first commit

---
+++ src/components/startMenu.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './startMenu.css'
+
+
+const StartMenu = () => {
+    return (
+        <div className='startMenuWrapper'>
+
+        </div>
+    )
+}
+
+
+export default StartMenu
