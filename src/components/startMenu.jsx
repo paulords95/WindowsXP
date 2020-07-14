d00dfea54172c708b8cfb301059663877e4ab207
paulords95 <paulords95@gmail.com>
2020-07-14 14:34:29 -0300

    fixed incompatible issues

--- src/components/startMenu.jsx
+++ src/components/startMenu.jsx
@@ -1,74 +1,100 @@
-import React, {useState} from 'react'
-import './startMenu.css'
-import github from '../imgs/github.png'
-import linkedin from '../imgs/linkedin.png'
-import avatar from '../imgs/avatar.jfif'
-import waldo from '../imgs/wally.png'
-
-
+import React, { useState } from "react";
+import "./startMenu.css";
+import github from "../imgs/github.png";
+import linkedin from "../imgs/linkedin.png";
+import avatar from "../imgs/avatar.jfif";
+import waldo from "../imgs/wally.png";
 
 const StartMenu = () => {
-    const [hover, setHover] = useState(false)
+  const [hover, setHover] = useState(false);
 
-
-    return (
-        <div className='startMenuWrapper'>
-            <div className='userName noselect'>
-                <img alt='avatar' id='userAvatar' src={avatar}></img>
-                <h1 id='avatarName'>Paulo Ricardo da Silva</h1>
+  return (
+    <div className="startMenuWrapper">
+      <div className="userName noselect">
+        <img alt="avatar" id="userAvatar" src={avatar}></img>
+        <h1 id="avatarName">Paulo Ricardo da Silva</h1>
+      </div>
+      <div className="startContentLeft noselect">
+        <ul>
+          <li
+            className="itemList"
+            onClick={() => {
+              let url = "https://github.com/paulords95";
+              const win = window.open(url, "_blank");
+              win.focus();
+            }}
+          >
+            <img alt="logo" id="gh" src={github}></img>
+            <div className="titleWrap">
+              <h1 className="leftTitle">GitHub</h1>
+              <h2 className="leftDescription">Meu GitHub</h2>
             </div>
-            <div className='startContentLeft noselect'>
-                <ul>
-                    <li className='itemList' onClick={()=> {
-                        let url = 'https://github.com/paulords95'
-                        const win = window.open(url, '_blank');
-                        win.focus();
-                    }}>
-                        <img alt='logo' id='gh' src={github}></img>
-                        <div className='titleWrap'>
-                            <h1 className='leftTitle'>GitHub</h1>
-                            <h2 className='leftDescription'>Meu GitHub</h2>
-                        </div>
-                    </li>
-                    <li className='itemList' onClick={()=> {
-                        let url = 'https://www.linkedin.com/in/paulo-ricardo-97360573/'
-                        const win = window.open(url, '_blank');
-                        win.focus();
-                    }}>
-                        <img alt='logo' id='linkedin' src={linkedin}></img>
-                        <div className='titleWrap'>
-                            <h1 className='leftTitle'>Linkedin</h1>
-                            <h2 className='leftDescription'>Meu Linkedin</h2>
-                        </div>
-                    </li>
-                    <hr></hr>
-                    <li className='itemList projects'>
-                        <div className='titleWrap'>
-                            <h1 className='leftTitle '>Projetos</h1>
-                        </div>
-                    </li>
-                    <li className='itemList waldo' onMouseLeave={()=> {
-                            setHover(false)
-                    }} onMouseOver={()=> {
-                        if (hover === false) {
-                            setHover(true)
-                        }
-                    }} >
-                        <img alt='logo' id='linkedin' src={waldo}></img>
-                        <div className='titleWrap'>
-                            <a target="_blank" rel="noopener noreferrer" href='https://paulords95.github.io/WhereIsWaldo/' className='leftTitle'>Onde está Waldo?</a>
-                        </div>
-                        <a alt='link' target="_blank" rel="noopener noreferrer" href='https://github.com/paulords95/WhereIsWaldo' className={` ${hover ? 'infoTextShow' : 'infoTextHide'}`}>Repositório no GitHub
-                            </a>
-                        <div className={`${hover ? 'waldoInfo' : ''}`}>
-                        </div>
-                    </li>
-                </ul>
+          </li>
+          <li
+            className="itemList"
+            onClick={() => {
+              let url = "https://www.linkedin.com/in/paulo-ricardo-97360573/";
+              const win = window.open(url, "_blank");
+              win.focus();
+            }}
+          >
+            <img alt="logo" id="linkedin" src={linkedin}></img>
+            <div className="titleWrap">
+              <h1 className="leftTitle">Linkedin</h1>
+              <h2 className="leftDescription">Meu Linkedin</h2>
             </div>
-            <div className='startContentRight'></div>
-        </div>
-    )
-}
+          </li>
+          <hr></hr>
+          <li className="itemList projects">
+            <div className="titleWrap">
+              <h1 className="leftTitle ">Projetos</h1>
+            </div>
+          </li>
+          <li
+            className="itemList waldo"
+            onMouseLeave={() => {
+              setHover(false);
+            }}
+            onMouseOver={() => {
+              if (hover === false) {
+                setHover(true);
+              }
+            }}
+          >
+            <img alt="logo" id="linkedin" src={waldo}></img>
+            <div className="titleWrap">
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://paulords95.github.io/WhereIsWaldo/"
+                className="leftTitle"
+              >
+                Onde está Waldo?
+              </a>
+              <a
+                alt="link"
+                id="waldoCode"
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://github.com/paulords95/WhereIsWaldo"
+              > </a>
+            </div>
+            <a
+              alt="link"
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://github.com/paulords95/WhereIsWaldo"
+              className={` ${hover ? "infoTextShow" : "infoTextHide"}`}
+            >
+              Repositório no GitHub
+            </a>
+            <div className={`${hover ? "waldoInfo" : ""}`}></div>
+          </li>
+        </ul>
+      </div>
+      <div className="startContentRight"></div>
+    </div>
+  );
+};
 
-
-export default StartMenu
+export default StartMenu;
--- src/components/startMenuBar.jsx
+++ src/components/startMenuBar.jsx
@@ -1,92 +1,81 @@
-import React, {useState} from 'react'
-import './startMenuBar.css'
-import logo from '../imgs/logo.png'
-import soundlogo from '../imgs/soundlogo.png'
-import defender from '../imgs/defender.png'
-import StartMenu from './startMenu'
-
-
+import React, { useState } from "react";
+import "./startMenuBar.css";
+import logo from "../imgs/logo.png";
+import soundlogo from "../imgs/soundlogo.png";
+import defender from "../imgs/defender.png";
+import StartMenu from "./startMenu";
 
 const updateTime = () => {
-    let date = new Date()
+  let date = new Date();
 
-    const hour = () => {
-        if (date.getHours() < 10) {
-            return (
-                '0'+date.getHours().toString()
-            )
-        } else {
-            return date.getHours()
-        }
+  const hour = () => {
+    if (date.getHours() < 10) {
+      return "0" + date.getHours().toString();
+    } else {
+      return date.getHours();
     }
+  };
 
-    const minutes = () => {
-        if (date.getMinutes() < 10) {
-            return (
-                '0'+date.getMinutes().toString()
-            )
-        } else {
-            return date.getMinutes()
-        }
+  const minutes = () => {
+    if (date.getMinutes() < 10) {
+      return "0" + date.getMinutes().toString();
+    } else {
+      return date.getMinutes();
     }
+  };
 
+  return hour() + ":" + minutes();
+};
 
-    return  hour()+":"+minutes()
-}
+const MenuBar = (props) => {
+  const [time, setTime] = useState({
+    time: updateTime(),
+  });
 
+  const [startMenu, setStartMenu] = useState(false);
 
-const MenuBar = () => {
-    
-    const [time, setTime] = useState({
-        time: updateTime()
-    })
+  setTimeout(() => {
+    setTime({
+      time: updateTime(),
+    });
+  }, 1000);
 
-    const [startMenu, setStartMenu] = useState(false)
+  const RenderStarMenu = () => {
+    if (startMenu) {
+      console.log('foi')
+      return <StartMenu />;
+    }
+  };
 
-   setTimeout(()=> {
-      setTime({
-          time: updateTime()
-      })
-   }, 1000)
+  return (
+    <div className="container">
+      <div className='desktop' onClick={()=> {
+        setStartMenu(false)
+      }}></div>
+      <div>{RenderStarMenu()}</div>
+      <div className="menubar">
+        <div
+          className="startMenu noselect"
+          onClick={() => {
+            if (startMenu) {
+              setStartMenu(false);
+            } else {
+              setStartMenu(true);
 
-   const RenderStarMenu = () => {
-      if (startMenu) {
-        return (
-            <StartMenu/>
-       )
-      } 
-   }
-
-    return (
-        <div className='container'>
-
-
-            <div>
-                {RenderStarMenu()}
-            </div>
-
-
-            <div className='menubar'>
-                <div className='startMenu noselect' onClick={()=> {
-                    if (startMenu) {
-                        setStartMenu(false)
-                    } else {
-                        setStartMenu(true)
-                    }
-                }}>
-                    <img alt='logo' src={logo}></img>
-                    <h1>Inciar</h1>
-                </div>
-                <div className='toolsWrap'>
-                    <img alt='defender' id='defender' src={defender}></img>
-                    <img alt='sound' id='sound' src={soundlogo}></img>
-                    <div>{time.time}</div>
-                </div>
-            </div>
+            }
+          }}
+        >
+          <img alt="logo" src={logo}></img>
+          <h1>Inciar</h1>
         </div>
-    )
-}
+        <div className="toolsWrap">
+          <img alt="defender" id="defender" src={defender}></img>
+          <img alt="sound" id="sound" src={soundlogo}></img>
+          <div>{time.time}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-
-
-export default MenuBar
+export default MenuBar;
