db7c0213a90ce6bfdfb53e5be53733d5766c5d4f
paulords95 <paulords95@gmail.com>
2020-07-20 15:28:10 -0300

    added notepad icon

--- src/App.js
+++ src/App.js
@@ -1,15 +1,57 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 
 import StartBar from "./components/startMenuBar";
 
 import Resize from "./components/resize";
+import notepadLogo from "./imgs/notepad.png";
 
 function App() {
+
+  const [dclick, setDclick] = useState(false);
+
+
+  const renderResize = () => {
+   if (dclick) {
+     return (
+       <div className='notepadRender'>
+         <Resize/>
+
+       </div>
+     )
+   }
+
+   if(!dclick) {
+     return (
+      <div></div>
+     )
+   }
+  }
+
   return (
     <div className="App">
+
+  <div className='appSpace'>{renderResize()}</div>
       <StartBar />
-      <Resize />
+
+      <div className="containerIcon">
+  
+      <img
+        src={notepadLogo}
+        alt="logo"
+        onDoubleClick={() => {
+          if (dclick) {
+            setDclick(false);
+          }
+
+          if (!dclick) {
+            setDclick(true);
+          }
+        }}
+        className={`notepadNormal ${dclick ? "notepadClicked" : ""}`}
+      ></img>
+
+    </div>
     </div>
   );
 }
