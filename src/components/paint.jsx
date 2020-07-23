30208cda03b27af7d5272e564a5e0b6d52834fdf
paulords95 <paulords95@gmail.com>
2020-07-23 11:41:25 -0300

    added possibility of color change

--- src/components/paint.jsx
+++ src/components/paint.jsx
@@ -1,11 +1,22 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import "./paint.css";
 
 
 
 const Paint = () => {
-  const [color, setColor] = useState('One')
+  const [color, setColor] = useState('#010000')
   
+  const colorChange = (clr) => {
+    setColor(clr)
+    const ctx = document.querySelector(".canvas").getContext("2d");
+    console.log(color)
+    ctx.strokeStyle = color
+    
+  }
+
+  useEffect(()=> {
+      colorChange(color)
+  }, [color])
 
   window.addEventListener("load", () => {
     const canvas = document.querySelector(".canvas");
@@ -30,19 +41,11 @@ const Paint = () => {
       ctx.beginPath();
     };
   
-    const draw = (e) => {
+    const draw = (e, clr) => {
       if (!painting) return;
       ctx.lineWidth = "10";
-      ctx.lineCap = "round";
-      setInterval(()=> {
-        if (color.active === "One") {
-          ctx.strokeStyle = '#010000'
-        }
-        if (color.active === "Two") {
-          ctx.strokeStyle = '#858182'
-        }
-  
-      }, 50)
+      ctx.lineCap = "round"
+    
       
   
       var xy = getMousePos(e);
@@ -83,7 +86,9 @@ const Paint = () => {
   };
   
   window.addEventListener("resize", setSize);
-
+  useEffect (()=> {
+    document.querySelector('.selectedColor').style.backgroundColor = color
+  })
 
   return (
     <div className="paintContainer">
@@ -93,38 +98,38 @@ const Paint = () => {
           <div className="drawingSelector"></div>
           <div className="colorSelector">
             <div className="colorPallet">
-              <div id="one" onClick={()=> setColor('One')}></div>
-              <div id="two" onClick={()=> setColor('Two')}></div>
-              <div id="three"></div>
-              <div id="four" ></div>
-              <div id="five"></div>
-              <div id="six"></div>
-              <div id="seven"></div>
-              <div id="eigth"></div>
-              <div id="nine"></div>
-              <div id="ten"></div>
-              <div id="eleven"></div>
-              <div id="twelve"></div>
-              <div id="thirteen"></div>
-              <div id="fourteen"></div>
-              <div id="fifteen"></div>
-              <div id="sixteen"></div>
-              <div id="seventeen"></div>
-              <div id="eighteen"></div>
-              <div id="nineteen"></div>
-              <div id="twenty"></div>
-              <div id="twentyOne"></div>
-              <div id="twentyTwo"></div>
-              <div id="twentyThree"></div>
-              <div id="twentyFour"></div>
-              <div id="twentyFive"></div>
-              <div id="twentySix"></div>
-              <div id="twentySeven"></div>
-              <div id="twentyEight"></div>
-              <div id="twentyNine"></div>
-              <div id="thirty"></div>
-              <div id="thirtyOne"></div>
-              <div id="thirtyTwo"></div>
+              <div id="one" onClick={()=> {colorChange('#010000')}}></div>
+              <div id="two" onClick={()=> {colorChange('#858182')}}></div>
+              <div id="three" onClick={()=> {colorChange('#8C0212')}}></div>
+              <div id="four" onClick={()=> {colorChange('#EE221B')}}></div>
+              <div id="five" onClick={()=> {colorChange('#E2EEFC')}}></div>
+              <div id="six" onClick={()=> {colorChange('#FFF410')}}></div>
+              <div id="seven" onClick={()=> {colorChange('#21B350')}}></div>
+              <div id="eigth" onClick={()=> {colorChange('#00A7E6')}}></div>
+              <div id="nine" onClick={()=> {colorChange('#08A5F4')}}></div>
+              <div id="ten" onClick={()=> {colorChange('#404AD0')}}></div>
+              <div id="eleven" onClick={()=> {colorChange('#A949AD')}}></div>
+              <div id="twelve" onClick={()=> {colorChange('#FFFDFC')}}></div>
+              <div id="thirteen" onClick={()=> {colorChange('#C9C7C8')}}></div>
+              <div id="fourteen" onClick={()=> {colorChange('#BD7C60')}}></div>
+              <div id="fifteen" onClick={()=> {colorChange('#FFAFD0')}}></div>
+              <div id="sixteen" onClick={()=> {colorChange('#F5D504')}}></div>
+              <div id="seventeen" onClick={()=> {colorChange('#F5E8B4')}}></div>
+              <div id="eighteen" onClick={()=> {colorChange('#BAE345')}}></div>
+              <div id="nineteen" onClick={()=> {colorChange('#9FDEF0')}}></div>
+              <div id="twenty" onClick={()=> {colorChange('#6F98C6')}}></div>
+              <div id="twentyOne" onClick={()=> {colorChange('#CCC3EE')}}></div>
+              <div id="twentyTwo" onClick={()=> {colorChange('#7387fb')}}></div>
+              <div id="twentyThree" onClick={()=> {colorChange('#9f962c')}}></div>
+              <div id="twentyFour" onClick={()=> {colorChange('#f77f1d')}}></div>
+              <div id="twentyFive" onClick={()=> {colorChange('#588e6e')}}></div>
+              <div id="twentySix" onClick={()=> {colorChange('#99f4d6')}}></div>
+              <div id="twentySeven" onClick={()=> {colorChange('#54a5d0')}}></div>
+              <div id="twentyEight" onClick={()=> {colorChange('#8b3609')}}></div>
+              <div id="twentyNine" onClick={()=> {colorChange('#6eca59')}}></div>
+              <div id="thirty" onClick={()=> {colorChange('#ceb37f')}}></div>
+              <div id="thirtyOne" onClick={()=> {colorChange('#550ad2')}}></div>
+              <div id="thirtyTwo" onClick={()=> {colorChange('#4668ea')}}></div>
             </div>
           </div>
           <div className="selectedColor"></div>
